d9775af928b1b646951d03c1936f3e688476147b
Quentin Lowette <quentin.lowette@gmail.com>
2020-04-20 11:50:55 +0200

    read letters function

--- code/solver.c
+++ code/solver.c
@@ -8,11 +8,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+/**
+ * Reads the file from the given path and returns an array of letter
+ */
+char *readLetters(const char *path)
+{
+	// Opens file
+	FILE *file = fopen(path, "r");
 
+	// If open fails
+	if (file == NULL)
+	{
+		printf("ERROR: Could not open the letters file.\n");
+		exit(-1);
+	}
+
+	size_t lineLength = 4;
+	char line[lineLength];
+	char *letters = NULL;
+	int counter = 0;
+
+	// Reads 1 char for each line
+	while (fgets(line, lineLength, file))
+	{
+		// Allocate memory
+		letters = realloc(letters, (counter+1) * sizeof(char));
+
+		// Copies the read letter into the array
+		letters[counter] = line[0];
+
+		// Increments counter
+		counter++;
+	}
+
+	return letters;
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	if (argc != 4)
+	{
+		printf("ERROR: Wrong number of arguments.\n");
+		exit(-1);
+	}
+
+	// const char *pathToWords = argv[1];
+	const char *pathToLetters = argv[2];
+	// const char *pathToScores = argv[3];
+
+	char *letters = readLetters(pathToLetters);
+
+	printf("Letters : %s\n", letters);
+
 	return 0;
 }
